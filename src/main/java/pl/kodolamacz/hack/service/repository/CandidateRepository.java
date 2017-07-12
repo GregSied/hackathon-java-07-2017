6115afaefacc03a44d04b73c4a779127dc9fb531
Justyna Sałacińska <salacinska.justyna@gmail.com>
2017-07-12 12:21:47 +0200

    added method to repositories

--- src/main/java/pl/kodolamacz/hack/service/repository/CandidateRepository.java
+++ src/main/java/pl/kodolamacz/hack/service/repository/CandidateRepository.java
@@ -7,4 +7,13 @@ import pl.kodolamacz.hack.model.Employer;
  * Created by 8760w on 2017-07-12.
  */
 public interface CandidateRepository extends CrudRepository<Candidate, Long> {
+
+
+    Candidate findById(Long id);
+
+    List<Candidate> findByName(String name);
+
+    @Query("select c from candidate where c.[name] like %?1%")
+    public List<Candidate> getByNameLike(String search);
+
 }
--- src/main/java/pl/kodolamacz/hack/service/repository/EmployerRepository.java
+++ src/main/java/pl/kodolamacz/hack/service/repository/EmployerRepository.java
@@ -3,8 +3,18 @@ package pl.kodolamacz.hack.service.repository;
 import org.springframework.data.repository.CrudRepository;
 import pl.kodolamacz.hack.model.Employer;
 
+import java.util.List;
+
 /**
  * Created by Jasiek on 12.07.2017.
  */
 public interface EmployerRepository extends CrudRepository<Employer, Long> {
+
+    Employer findById(Long id);
+
+    List<Employer> findByName(String name);
+
+    @Query("select e from employer where e.[name] like %?1%")
+    public List<Employer> getByNameLike(String search);
+
 }
