8b56a6b093f9aad320b92d37222dfa6cfcd95160
Błażej Norys <b.norys@gmail.com>
2017-07-12 13:29:57 +0200

    Update candidate service

--- src/main/java/pl/kodolamacz/hack/model/Candidate.java
+++ src/main/java/pl/kodolamacz/hack/model/Candidate.java
@@ -14,9 +14,57 @@ public class Candidate {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
-    String firstName;
-    String lastName;
+    String candidateName;
+    String candidateSurname;
     int age;
     String hobbies;
     String candidateEmail;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCandidateName() {
+        return candidateName;
+    }
+
+    public void setCandidateName(String candidateName) {
+        this.candidateName = candidateName;
+    }
+
+    public String getCandidateSurname() {
+        return candidateSurname;
+    }
+
+    public void setCandidateSurname(String candidateSurname) {
+        this.candidateSurname = candidateSurname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    public String getCandidateEmail() {
+        return candidateEmail;
+    }
+
+    public void setCandidateEmail(String candidateEmail) {
+        this.candidateEmail = candidateEmail;
+    }
 }
