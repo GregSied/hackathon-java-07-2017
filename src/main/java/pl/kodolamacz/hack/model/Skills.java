52fb5e4fd5210d7c409f027ee6f2f3d7df226791
Mikołaj Trojnar <mikolaj.trojnar@gmail.com>
2017-07-12 13:00:34 +0200

    model 1.1

--- src/main/java/pl/kodolamacz/hack/model/Skills.java
+++ src/main/java/pl/kodolamacz/hack/model/Skills.java
@@ -1,8 +1,15 @@
 package pl.kodolamacz.hack.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * 2017-07-12.
  */
+
+@Entity
+@Table(name = "skills")
+
 public enum Skills {
 
     JAVA, JAVASCRIPT, HTML, CSS, ANGULAR, SPRING, HIBERNATE, SQL
