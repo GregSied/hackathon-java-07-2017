fe81eb129445c4134bdb2e74552ae1e7a8ba7680
Radosław Szmit <radoslawszmit@gmail.com>
2017-07-13 15:49:36 +0200

    Merge

--- src/main/java/pl/kodolamacz/hack/controller/CandidateController.java
+++ src/main/java/pl/kodolamacz/hack/controller/CandidateController.java
@@ -65,23 +65,20 @@ public class CandidateController {
                 "candidateViews/candidateProfile","candidate", candidate);
     }
 
-
     //UPDATE CANDIDATE GET METHOD
-<<<<<<< HEAD
     @RequestMapping(value = "/update", method = RequestMethod.GET)
-    public ModelAndView showUpdateCandidate(@RequestParam long id){
+    public ModelAndView showUpdateCandidate(@RequestParam long id) {
         Map<String, Object> paramters = new HashMap<>();
         paramters.put("candidate", candidateService.findCandidateProfileId(id));
         return new ModelAndView("candidateViews/editCandidateForm", paramters);
+    }
 
-=======
     @RequestMapping(value = "updateCandidate.html", method = RequestMethod.GET)
     public ModelAndView showUpdateCandidate(){
         Map<String, Object> parameters = new HashMap<>();
         Long id = SecurityContext.getCurrentlyLoggedUser().getId();
         parameters.put("candidate", candidateService.findCandidateProfileId(id));
         return new ModelAndView("candidateViews/editCandidateForm", parameters);
->>>>>>> 153baa72dd109bd70abcae3271771778340e6eb3
     }
 
     //UPDATE CANDIDATE POST METHOD
