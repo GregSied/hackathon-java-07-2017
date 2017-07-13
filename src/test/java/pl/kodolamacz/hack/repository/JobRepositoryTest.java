5874cf65d272c14a7e0442437301cba75e2c3589
Justyna Sałacińska <salacinska.justyna@gmail.com>
2017-07-13 13:53:49 +0200

    Job repository save method test

---
+++ src/test/java/pl/kodolamacz/hack/repository/JobRepositoryTest.java
@@ -0,0 +1,53 @@
+package pl.kodolamacz.hack.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import pl.kodolamacz.hack.model.Candidate;
+import pl.kodolamacz.hack.model.Employer;
+import pl.kodolamacz.hack.model.Job;
+import pl.kodolamacz.hack.model.User;
+import pl.kodolamacz.hack.service.repository.CandidateRepository;
+import pl.kodolamacz.hack.service.repository.EmployerRepository;
+import pl.kodolamacz.hack.service.repository.JobRepository;
+import pl.kodolamacz.hack.service.repository.UserRepository;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 8760w on 2017-07-13.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring.xml")
+public class JobRepositoryTest extends AbstractTransactionalJUnit4SpringContextTests {
+
+    @Autowired
+    private JobRepository jobRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @Test
+    public void shouldSaveJob() throws Exception {
+        //given
+        User user = User.admin("admin", "root123");
+        userRepository.save(user);
+        Employer employer = new Employer("karol", "karol@mail.com", "Krakow", 20, user);
+        employerRepository.save(employer);
+        Job job = new Job(employer.getId(),"gardener", "watering", "flower", "nothing", 10, 20);
+        //when
+        jobRepository.save(job);
+        //then
+        Assertions.assertThat(job.getId()).isNotNull();
+
+    }
+
+
+}
